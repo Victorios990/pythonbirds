0a7383666743cd2010810e58cf89c046347f46a7
55619 <victor.laurentino90@gmail.com>
2024-11-07 10:26:24 -0300

    Criados atributos de instancia (nome e idade)

--- OrientaçãoObjeto/pessoa.py
+++ OrientaçãoObjeto/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, idade=34):
+        self.idade = 34
+        self.nome = None
     def comprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('José')
     print(Pessoa.comprimentar(p))
     print(id(p))
     print(p.comprimentar())
+    print(p.nome)
+    p.nome = 'Victor'
+    print(p.nome)
+    print(p.idade)
