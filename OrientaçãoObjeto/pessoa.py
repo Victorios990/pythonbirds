9828b2721b0b5b16795b831cc559b2e0cbfd944a
55619 <victor.laurentino90@gmail.com>
2024-11-07 13:49:13 -0300

    Atualizada com metodos

--- OrientaçãoObjeto/pessoa.py
+++ OrientaçãoObjeto/pessoa.py
@@ -1,15 +1,30 @@
 class Pessoa:
-    def __init__(self, idade=34, nome=None, *filhos):
+    def __init__(self, idade=34, nome=None, *filhos, email='@python.com', olhos = 2):
         self.idade = 34
         self.nome = None
         self.filhos= list(filhos)
+        self.email = email
+        self.olhos = 2
 
     def comprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_sobrenome_de_classe(cls):
+        return f'{cls} - olhos'
+
+
 if __name__ == '__main__':
     jose = Pessoa(nome='José')
     victor = Pessoa(jose, nome='Victor')
+#    email = '@python.com'
+    emailjose = jose.email
+    emailvictor = victor.email
+    print(jose.email)
     print(Pessoa.comprimentar(jose))
     print(Pessoa.comprimentar(victor))
     print(id(jose))
@@ -20,7 +35,12 @@ if __name__ == '__main__':
     for filho in victor.filhos:
         print(filho.nome)
     jose.sobrenome = 'Almeida'
-    print(jose.sobrenome)
+    del jose.olhos
+    print(jose.sobrenome,emailjose)
     print(jose.__dict__)
     print(victor.__dict__)
+    print(Pessoa.metodo_estatico())
+
+
+
 
